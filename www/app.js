534573da991477ab754587244151c8587c4037dc
server <user@ubuntu.(none)>
2013-12-29 12:35:57 -0500

    added new view/page info

--- www/app.js
+++ www/app.js
@@ -73,56 +73,57 @@ var get_data_browser = function(){
 var load_cifras = (/Chrome/i.test(navigator.userAgent) ? get_data_browser() : get_data_mobile());
 
 app.view.page = Backbone.View.extend({
-  //DEFAULTS
-  el: 'main'
-  ,name: 'SET page NAME, CURRENTLY COMING FROM PARENT'
-  ,initialize:function(options){
-    //initialize runs only once when object is initiated
-    this.clear_page();
-    this.options = options;
-    app.url = document.URL;
-    app.file = location.pathname;
-    _.bindAll(this, 'render');
-    this.render();
-  }
-  ,set_URN:function(){
-    app.URN.previous = app.URN.current;
-    app.URN.current = location.hash;
-  }
-  ,change_page:function(ev){
-      $('.controls li').css('background','rgb(0, 26, 3)');
+    //DEFAULTS
+    el: 'main'
+    ,name: 'SET page NAME, CURRENTLY COMING FROM PARENT'
+    ,initialize:function(options){
+        //initialize runs only once when object is initiated
+        this.clear_page(); //this may be the cause of app showing blank as it boots up
+        this.options = options;
+        app.url = document.URL;
+        app.file = location.pathname;
+        _.bindAll(this, 'render');
+        this.render();
+    }
+    ,set_URN:function(){
+        app.URN.previous = app.URN.current;
+        app.URN.current = location.hash;
+    }
+    ,change_page:function(ev){
+        $('.controls li').css('background','rgb(0, 26, 3)');
 
-       this.record_history(ev.target.id-1);
-       app.cifra.id = ev.target.id-1;
-       if(ev.target.className == 'first_line'){
+        this.record_history(ev.target.id-1);
+        app.cifra.id = ev.target.id-1;
+        if(ev.target.className == 'first_line'){
           app.route.navigate('cifra/'+app.cifra.id, {trigger: true});
-       }
-       else{
+        }
+        else{
           app.route.navigate(ev.target.id, {trigger: true});
           // console.log($(ev.target).html());
           //controls css
           $(ev.target).css('background','grey');
-       }
-       $('body').animate({ scrollTop: 0 }, 0); //scroll to the top of page
+        }
+        $('body').animate({ scrollTop: 0 }, 0); //scroll to the top of page
 
-  }
-   ,record_history:function(id){ 
-       if( isNaN(id) || id == -1){ //only record the cifra pages that has an id set to a number greater than -1
+    }
+    ,record_history:function(id){ 
+        if( isNaN(id) || id == -1){ //only record the cifra pages that has an id set to a number greater than -1
           return false;
-       }
-       var history =  app.cifra.history;
-       var i = history.indexOf(id)
-       if( i != -1){ //no duplicates
+        }
+        var history =  app.cifra.history;
+        var i = history.indexOf(id)
+        if( i != -1){ //no duplicates
           history.splice(i, 1);
-       }
-       history.unshift(id); 
-  }
-  ,clear_page:function(){
-    var deferred = new $.Deferred();
-    this.$el.html('');
-    deferred.resolve('hello world');
-    return deferred;
-  }
+        }
+        history.unshift(id); 
+    }
+    ,clear_page:function(){
+        // alert('page was cleared');
+        var deferred = new $.Deferred();
+        this.$el.html('');
+        deferred.resolve('hello world');
+        return deferred;
+    }
 });
 
   app.view.controls = app.view.page.extend({
@@ -141,17 +142,17 @@ app.view.page = Backbone.View.extend({
           //when you add a page route is should automatically be created a page
           ul.append('<li id="">Alpha</li>');      
           ul.append('<li id="history">History</li>');      
-          ul.append('<li id="history">Info</li>');      
+          ul.append('<li id="info">Info</li>');      
           ul.append('<li id="version">v2.0.0</li>');      
       }
   });
 
 app.view.nav_list = app.view.page.extend({
-   render: function(){
-    this.set_URN();
-    this.draw_list();
-    return this;
-  }
+    render: function(){
+        this.set_URN();
+        this.draw_list();
+        return this;
+  } 
   ,draw_list:function(){
     var that = this;
     var i = 1;
@@ -160,7 +161,6 @@ app.view.nav_list = app.view.page.extend({
       i++;
     });
   }
-
 })
 
 
@@ -171,28 +171,38 @@ app.view.home_page = app.view.nav_list.extend({
   }
 });
 
- app.view.history_page = app.view.nav_list.extend({
-  //if you load the history page directly theres no events to make the navigation work
-      name: 'history'
-      ,draw_list:function(){
-           var that = this;
-           var history = app.cifra.history;
-           $.each(history, function(key, value){
-              if(value != undefined){
-                  that.$el.append('<li class="first_line" id=' + (value+1) + '>' + that.collection.at(value).get('first_line') + '</li>');
-              }
+app.view.history_page = app.view.nav_list.extend({
+ //if you load the history page directly theres no events to make the navigation work
+    name: 'history'
+    ,draw_list:function(){
+       var that = this;
+       var history = app.cifra.history;
+       $.each(history, function(key, value){
+          if(value != undefined){
+              that.$el.append('<li class="first_line" id=' + (value+1) + '>' + that.collection.at(value).get('first_line') + '</li>');
+          }
        });
-      }
- });
+    }     
+});
 
 app.view.cifra_page = app.view.page.extend({
-  name: 'cifra'
-  ,render: function(){
-    this.set_URN();
-    // $('body').animate({ scrollTop: 0 }, 0); //scroll to the top of page
-    this.$el.append('<pre>'+this.collection.at(app.cifra.id).get('html')+'</pre>');
-    return this;
-  }
+    name: 'cifra'
+    ,render: function(){
+        this.set_URN();
+        // $('body').animate({ scrollTop: 0 }, 0); //scroll to the top of page
+        this.$el.append('<pre>'+this.collection.at(app.cifra.id).get('html')+'</pre>');
+        return this;
+    }
+});
+
+app.view.info_page = app.view.page.extend({
+    name: 'info'
+    ,render: function(){
+        this.$el.append('<p>info page</p>');
+        this.$el.append('<p>any errors?</p>');
+        this.$el.append('<p>Is internet on?</p>');
+        return this;
+    }
 });
 
 app.collection.cifras = Backbone.Collection.extend({
@@ -200,42 +210,46 @@ app.collection.cifras = Backbone.Collection.extend({
 });
 
 app.router.music_stand = Backbone.Router.extend({
-  routes:{
-    // "route", "fuction name",
-    "": "draw_home",
-    "cifra/:id": "draw_cifra", //eg: URL/#/cifra/12
-    "history": "draw_history"
-  }
-  ,initialize:function(){
-      new app.view.controls(); 
-      app.data.collection = new app.collection.cifras(app.data.JSON);
-  } 
-  ,draw_home:function(){
-    this.initiate('home_page','{collection: app.data.collection}');
-  }
-  ,draw_cifra: function(id){
-    if(app.cifra['id'] == undefined){
-      app.route.navigate('', {trigger: true});
-      util_error('Custom Warning: app.cifra.id was not set.  May be because you arrived at the previous route incorrectly, therefore the page was redirected.\nFile: '+ app.file+'\nRoute:"'+app.URN.current+'"');
-      return false;
+    routes:{
+        // "route", "fuction name",
+        "": "draw_home",
+        "cifra/:id": "draw_cifra", //eg: URL/#/cifra/12
+        "history": "draw_history",
+        "info": "draw_info",
     }
-    this.initiate('cifra_page','{collection: app.data.collection}');
-  }
- ,draw_history:function(){
-    this.initiate('history_page','{collection: app.data.collection}');
- }
- ,initiate:function(view, param){
-    page = app.pages[view];
-    if(!page){  //exist?
-      app.pages[view] = eval('new app.view.'+ view + '('+param+')');
+    ,initialize:function(){
+          new app.view.controls(); 
+          app.data.collection = new app.collection.cifras(app.data.JSON);
+    } 
+    ,draw_home:function(){
+        this.render_view('home_page','{collection: app.data.collection}');
     }
-    else{
-       page.clear_page().done(function(n){
-          page.render();
-          // console.log(n);
-       });
+    ,draw_cifra: function(id){
+        if(app.cifra['id'] == undefined){
+          app.route.navigate('', {trigger: true});
+          util_error('Custom Warning: app.cifra.id was not set.  May be because you arrived at the previous route incorrectly, therefore the page was redirected.\nFile: '+ app.file+'\nRoute:"'+app.URN.current+'"');
+          return false;
+        }
+        this.render_view('cifra_page','{collection: app.data.collection}');
     }
-  }
+    ,draw_history:function(){
+        this.render_view('history_page','{collection: app.data.collection}');
+    }
+    ,draw_info:function(){
+        this.render_view('info_page');    
+    }
+    ,render_view:function(view, param){
+        page = app.pages[view];
+        if(!page){  //exist?
+          app.pages[view] = eval('new app.view.'+ view + '('+param+')');
+        }
+        else{
+           page.clear_page().done(function(n){
+              page.render();
+              // console.log(n);
+           });
+        }
+    }
 
 });
   
